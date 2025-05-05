6e70b94161357296a51229d7aea4bd00e3d741bd
yapmDev <yosmelperez128@gmail.com>
2025-05-05 15:01:04 -0400

    basic map view with user location

--- lib/common/pages/map.dart
+++ lib/common/pages/map.dart
@@ -1,11 +1,33 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
-  const MapPage({super.key});
+class MapPage extends StatefulWidget {
+  const MapPage({
+    super.key,
+    required this.position
+  });
+
+  final Position position;
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const MapWidget();
+    return MapWidget(
+      styleUri: MapboxStyles.STANDARD,
+      cameraOptions: CameraOptions(
+          center: Point(coordinates: widget.position),
+          pitch: 0,
+          bearing: 0,
+          zoom: 15
+      ),
+      onMapCreated: (controller){
+        controller.location.updateSettings(LocationComponentSettings(enabled: true));
+      }
+    );
   }
 }
