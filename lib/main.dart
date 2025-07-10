818a63dae877891a933ec4d7a3a57ff7b320376c
Ronny Nordelo <ronnynordelopena@gmail.com>
2025-07-10 04:21:04 -0400

    feat: Holding splash while the app is loading

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app.dart';
 import 'flavors.dart';
-
 import 'package:geolocator/geolocator.dart';
 import 'package:quber_taxi/config/api_config.dart';
 import 'package:quber_taxi/config/build_config.dart';
@@ -14,17 +14,18 @@ import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:quber_taxi/utils/websocket/core/websocket_service.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
 
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  //Initialization Block
   BuildConfig.loadConfig();
-
   await SharedPrefsManager.init();
-
   MapboxOptions.setAccessToken(ApiConfig().mapboxAccessToken);
-
   WebSocketService.instance.connect(baseUrl: ApiConfig().baseUrl);
-
   await Geolocator.requestPermission();
+
+  FlutterNativeSplash.remove();
 
   F.appFlavor = Flavor.values.firstWhere(
         (element) => element.name == appFlavor,
