9b9ec7a1443ea5f62fc5f99ee40cac47c7713338
yapmDev <yosmelperez128@gmail.com>
2025-07-10 22:50:56 -0400

    client settings func

--- lib/common/models/client.dart
+++ lib/common/models/client.dart
@@ -7,13 +7,33 @@ class Client implements Encodable{
   final int id;
   final String name;
   final String phone;
+  final String? profileImageUrl;
+  final String referralCode;
 
-  const Client({required this.id, required this.name, required this.phone});
+  const Client({
+    required this.id,
+    required this.name,
+    required this.phone,
+    this.profileImageUrl,
+    required this.referralCode
+  });
 
   @override
-  Map<String, dynamic> toJson() => {"id": id, "name": name, "phone": phone};
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "phone": phone,
+    "profileImageUrl": profileImageUrl,
+    "referralCode": referralCode
+  };
 
   factory Client.fromJson(Map<String, dynamic> json) {
-    return Client(id: json["id"], name: json["name"], phone: json["phone"]);
+    return Client(
+        id: json["id"],
+        name: json["name"],
+        phone: json["phone"],
+        profileImageUrl: json["profileImageUrl"],
+        referralCode: json["referralCode"]
+    );
   }
 }
