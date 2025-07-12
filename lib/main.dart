1787491e158d08155edb3b2c1d5503e6723d189c
yapmDev <yosmelperez128@gmail.com>
2025-07-12 13:23:12 -0400

    sync appProfile with flavors

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'app.dart';
-import 'flavors.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:quber_taxi/config/api_config.dart';
 import 'package:quber_taxi/config/build_config.dart';
 import 'package:quber_taxi/storage/prefs_manager.dart';
 import 'package:quber_taxi/utils/websocket/core/websocket_service.dart';
+
+import 'app.dart';
 
 Future<void> main() async {
 
@@ -24,10 +23,6 @@ Future<void> main() async {
   await Geolocator.requestPermission();
 
   FlutterNativeSplash.remove();
-
-  F.appFlavor = Flavor.values.firstWhere(
-        (element) => element.name == appFlavor,
-  );
 
   runApp(const App());
 }
